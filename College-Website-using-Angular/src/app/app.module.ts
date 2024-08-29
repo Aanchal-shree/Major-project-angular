32334b9120aef1b3c844e669f207688fa584e65f
Aanchal-shree <as.aanchalshree@gmail.com>
2024-08-29 13:51:41 +0530

    Student portal

--- College-Website-using-Angular/src/app/app.module.ts
+++ College-Website-using-Angular/src/app/app.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
 
   {path:'faq', component:FaqComponent},
   {path:'feestructure', component:FeestructureComponent},
-  {path:'career', component:CareerComponent}
+  {path:'career', component:CareerComponent},
 
   {path:'vc-desk', component:VcDeskComponent}
 
